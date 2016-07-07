022acfd0fd154be72eb6bbca61c842cffd69b4e7
temy13 <temy.poporofier00@gmail.com>
2016-07-07 10:36:41 +0900

    server connection

--- PresentationApp/MainPage.cs
+++ PresentationApp/MainPage.cs
@@ -8,25 +8,29 @@ namespace PresentationApp
 	{
 		public MainPage ()
 		{
+			Entry keyEntry = new Entry {Text = "KeyCode", Placeholder = "Input your KeyCode"};
 			//ボタンを生成
-			var audience_button = new Button { Text = "聴衆者" };
+			var audience_button = new Button { Text = "聴衆者!" };
 			//ボタンクリック時の処理
 			audience_button.Clicked += async (s, a) => {
 				//ページを遷移する
-				await Navigation.PushAsync(new AudienceMainPage());
+				ServerInfo server_info = DependencyService.Get<IHttpConnection>().GetServerByBroadCast(keyEntry.Text);
+				await Navigation.PushAsync(new AudienceMainPage(server_info));
 			};
 			//ボタンを生成
 			var presenter_button = new Button { Text = "発表者" };
 			//ボタンクリック時の処理
 			presenter_button.Clicked += async (s, a) => {
 				//ページを遷移する
-				await Navigation.PushAsync(new PresenterMainPage());
+				ServerInfo server_info = DependencyService.Get<IHttpConnection>().GetServerByBroadCast(keyEntry.Text);
+				await Navigation.PushAsync(new PresenterMainPage(server_info));
 			};
 
 			this.Content = new StackLayout {
 				Spacing = 0,
 				VerticalOptions = LayoutOptions.FillAndExpand,
 				Children = {
+					keyEntry,
 					audience_button,
 					presenter_button
 				}
